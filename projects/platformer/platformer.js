d9f49d24eb9e51861bb78204b7366e2402552977
Mau810 <157516733+Mau810@users.noreply.github.com>
2024-02-27 16:38:08 +0000

    add index.html and css files

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -43,28 +43,33 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-
-
-
-    
+    createPlatform(0, 250, 150, 10);
+    createPlatform(380, 380, 150, 10);
+    createPlatform(800, 475, 175, 10);
+    createPlatform(400, 575, 175, 10);
+    createPlatform(1200, 400, 150, 10);
+    createPlatform(150, 650, 150, 10);
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
-
-
-
+    createCollectable("database", 1200, 395, 150, 10);
     // TODO 3
     // Create cannons
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
-
-
-
+    
+    createCannon("top", 300, 500);  
+    createCannon("right", 300, 500);  
+    createCannon("bottom", 650, 750);
+    createCannon("bottom", 1050, 900);
+     
+      
+    
+    
+    
     /////////////////////////////////////////////////
     //////////ONLY CHANGE ABOVE THIS POINT///////////
     /////////////////////////////////////////////////
